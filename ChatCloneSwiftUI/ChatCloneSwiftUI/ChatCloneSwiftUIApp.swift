742dc3e2fde87791a09d0d2d3f32ed91f86c82c1
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-04-23 18:26:42 +0300

    init project

---
+++ ChatCloneSwiftUI/ChatCloneSwiftUI/ChatCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatCloneSwiftUIApp.swift
+//  ChatCloneSwiftUI
+//
+//  Created by Oleksandr Isaiev on 23.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
