7bb6ca1f6312bbe2e50402b516ece62c8546200e
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-04-29 14:46:03 +0300

    move mock service into separate file

--- ChatCloneSwiftUI/ChatCloneSwiftUI/ViewModel/MockAuthViewModel.swift
+++ ChatCloneSwiftUI/ChatCloneSwiftUI/ViewModel/MockAuthViewModel.swift
@@ -5,4 +5,14 @@
 //  Created by Oleksandr Isaiev on 29.04.2024.
 //
 
-import Foundation
+import UIKit
+
+final class MockAuthViewModel: NSObject, ObservableObject, AuthProtocol {
+    func logIn() {}
+
+    func register(withEmail email: String, password: String, fullName: String, userName: String) {}
+
+    func uploadProfileImage(_ image: UIImage) {}
+
+    func signOut() {}
+}
